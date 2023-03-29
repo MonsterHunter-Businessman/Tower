7b16ed7cfb971a6fb8436c9eb9e6899abd274e95
aprnald74 <a010313813321@gmail.com>
2023-03-29 23:53:23 +0900

    :white_check_mark:

--- Assets/Script/Monster.cs
+++ Assets/Script/Monster.cs
@@ -2,41 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
-using UnityEngine.UI;
-using Unity.VisualScripting;
+
+//public enum monastertype { goblin,slime}
 
 public class Monster : MonoBehaviour
 {
 
-    public Transform player;
-    public PathType pathsystem = PathType.CatmullRom;
-    public PathMode pathmode = PathMode.Ignore;
-    public int resulution = 10;
-    public Color gizmoColor = Color.red;
+    public Transform monster; // 몬스터 좌표
+    
+    public Vector3[] pathval = new Vector3[5]; // 이동 좌표
+    public float speed; // 이 시간안에 왕복
+    public PathType pathtype = PathType.CatmullRom; // 이동 방식
+    public PathMode pathmode; // 경로 모드
+    public int resolution; // 해상도
+    public Color gizmoColor; // 이동 경로 색 지정
 
-    public Vector3[] pathval = new Vector3[0];
+    public float fStartTime; // 이 시간 후에 시작
 
-    public float speed = 10;
+    // monastertype monasterType;
 
-    // Start is called before the first frame update
     void Start()
     {
-
-        player.transform.DOPath(pathval, speed, pathsystem, pathmode, resulution, gizmoColor);
+        gameObject.SetActive(false);
+        Invoke("move", fStartTime); //fStartTime시간 뒤에 move실행
     }
 
-    // Update is called once per frame
-    void Update()
+    void move()
     {
-        
+        gameObject.SetActive(true);
+        monster.transform.DOPath(pathval, speed, pathtype, pathmode, resolution, gizmoColor);
     }
-    void OnCollisionEnter(Collision col)
+
+    private void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.gameObject.tag == "Player")
+
+        if (col.gameObject.tag == "END")
         {
+            Debug.Log("뒤짐");
             Destroy(gameObject);
         }
-            
-    }
 
+    }
+    //public void Monastertype()
+    //{
+    //    case MonsterType(monastertype) {
+
+    //    }
+    //}
 }
